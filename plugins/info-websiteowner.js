86200c2e30af37ee4dc98c0912e13e2b129ab331
ཌ✼ʟᴇᴠɪ-ʏᴀɴ-xʜɪᴀᴏ✼ད <111663350+ImleviBotzz@users.noreply.github.com>
2022-09-23 07:24:53 +0800

    Update info-websiteowner.js

--- plugins/info-websiteowner.js
+++ plugins/info-websiteowner.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
     /*conn.sendTemplateButtonDoc(m.chat, col, `*Link Group:* ${await conn.getName(m.chat)}\n\nhttps://chat.whatsapp.com/` + await conn.groupInviteCode(m.chat), wm, 'Info Grup', '.infogrup', m)*/
 
 conn.sendMessage(m.chat, {
-  text: `DRAGON-BOTZ Website`,
+  text: `𝙍𝙮𝙪𝙪 𝘽𝙤𝙩𝙯𝙯 Website`,
   templateButtons: [{
     index: 1,
     urlButton: {
