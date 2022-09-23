9ed57cb88fa08ff3c265616984cf6e26e1c7994d
ཌ✼ʟᴇᴠɪ-ʏᴀɴ-xʜɪᴀᴏ✼ད <111663350+ImleviBotzz@users.noreply.github.com>
2022-09-23 08:33:32 +0800

    Update rules-maaf.js

--- plugins/rules-maaf.js
+++ plugins/rules-maaf.js
@@ -4,7 +4,7 @@ let text = `${htki} RULES ${htka}
 > Minta maaf ke owner agar tidak terkena pelanggaran 
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'Youtube', url: 'https://youtube.com/channel/UCACHvReRmw2fxgMutPFCBWg'}},
+    {index: 1, urlButton: {displayText: 'Youtube', url: 'https://youtube.com/channel/UC2GOH5mCArh3E4J3sGFMC5g'}},
 ]
 let tm = {
 text: text,
